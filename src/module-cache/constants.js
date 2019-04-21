83044500a23f715390ae3a92fa5fb4d23903c0cd
Jérôme Guyon <jeromep.guyon@gmail.com>
2019-04-21 17:20:30 +0200

    Fix symbol name of module cache constants

--- src/module-cache/constants.js
+++ src/module-cache/constants.js
@@ -5,6 +5,6 @@ export const MODULE_CHUNK_NAME = Symbol("MODULE_CHUNK_NAME");
 export const IS_MODULE_CACHE = Symbol("IS_MODULE_CACHE");
 export const MODULE_CACHE_FETCH = Symbol("MODULE_CACHE_FETCH");
 
-export const MODULE_PENDING = Symbol("MODULE_STATUS_PENDING");
-export const MODULE_SUCCESS = Symbol("MODULE_STATUS_SUCCESS");
-export const MODULE_FAILURE = Symbol("MODULE_STATUS_FAILURE");
+export const MODULE_PENDING = Symbol("MODULE_PENDING");
+export const MODULE_SUCCESS = Symbol("MODULE_SUCCESS");
+export const MODULE_FAILURE = Symbol("MODULE_FAILURE");
