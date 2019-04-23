192f6d20ae037c0e0b8dfeb9b4a6f79356f574e9
Jérôme Guyon <jeromep.guyon@gmail.com>
2019-04-23 16:49:14 +0200

    Unextract functions in resource cache
    Extracting functions needlessly produces functions with many arguments,
    it's not very readable.

--- src/resource-cache/create-client-resource-cache.js
+++ src/resource-cache/create-client-resource-cache.js
@@ -16,113 +16,110 @@ export default function createClientResourceCache(opts) {
   const entries = new Map();
   const lru = createLRU(opts);
 
-  return {
-    [IS_RESOURCE_CACHE]: true,
-    [RESOURCE_CACHE_FETCH]: (resource, input) =>
-      fetchEntry(entries, lru, resource, input),
-    preload: (resource, input) => preloadEntry(entries, lru, resource, input),
-  };
-}
+  function fetch(resource, input) {
+    invariant(
+      resource && typeof resource === "object" && resource[IS_RESOURCE],
+      "expected resource to be an object returned by `createResource`",
+    );
 
-function fetchEntry(entries, lru, resource, input) {
-  invariant(
-    resource && typeof resource === "object" && resource[IS_RESOURCE],
-    "expected resource to be an object returned by `createResource`",
-  );
+    const resourceEntries = getResourceEntries(resource);
+    const hash = resource[RESOURCE_HASH](input);
+    const entry = resourceEntries.get(hash);
 
-  const resourceEntries = getResourceEntries(entries, resource);
-  const hash = resource[RESOURCE_HASH](input);
-  const entry = resourceEntries.get(hash);
+    if (entry) {
+      const value = lru.access(entry);
 
-  if (entry) {
-    const value = lru.access(entry);
-
-    if (value === LRU_EVICTED_ENTRY) {
+      if (value === LRU_EVICTED_ENTRY) {
+        return createResourceEntry(
+          resourceEntries,
+          hash,
+          resource[RESOURCE_FETCH](input),
+        );
+      } else {
+        return value;
+      }
+    } else {
       return createResourceEntry(
         resourceEntries,
-        lru,
         hash,
         resource[RESOURCE_FETCH](input),
       );
-    } else {
-      return value;
     }
-  } else {
-    return createResourceEntry(
-      resourceEntries,
-      lru,
-      hash,
-      resource[RESOURCE_FETCH](input),
+  }
+
+  function getResourceEntries(resource) {
+    const resourceEntries = entries.get(resource);
+
+    if (resourceEntries) {
+      return resourceEntries;
+    } else {
+      const newResourceEntries = new Map();
+      entries.set(resource, newResourceEntries);
+      return newResourceEntries;
+    }
+  }
+
+  function createResourceEntry(resourceEntries, hash, promise) {
+    const resultPromise = promise.then(
+      data => {
+        const successResult = {
+          status: RESOURCE_SUCCESS,
+          data,
+        };
+
+        lru.update(entry, successResult);
+        return successResult;
+      },
+      error => {
+        const failureResult = {
+          status: RESOURCE_FAILURE,
+          error,
+        };
+
+        lru.update(entry, failureResult);
+        return failureResult;
+      },
     );
+
+    const pendingResult = {
+      status: RESOURCE_PENDING,
+      listen: cb => {
+        let isCanceled = false;
+
+        resultPromise.then(result => {
+          if (!isCanceled) {
+            cb(result);
+          }
+        });
+
+        return () => {
+          isCanceled = true;
+        };
+      },
+    };
+
+    const entry = lru.add(pendingResult, () => resourceEntries.delete(hash));
+    resourceEntries.set(hash, entry);
+    return pendingResult;
   }
-}
 
-function getResourceEntries(entries, resource) {
-  const resourceEntries = entries.get(resource);
+  function preload(resource, input) {
+    const result = fetch(resource, input);
 
-  if (resourceEntries) {
-    return resourceEntries;
-  } else {
-    const newResourceEntries = new Map();
-    entries.set(resource, newResourceEntries);
-    return newResourceEntries;
-  }
-}
-
-function createResourceEntry(resourceEntries, lru, hash, promise) {
-  const resultPromise = promise.then(
-    data => {
-      const successResult = {
-        status: RESOURCE_SUCCESS,
-        data,
-      };
-
-      lru.update(entry, successResult);
-      return successResult;
-    },
-    error => {
-      const failureResult = {
-        status: RESOURCE_FAILURE,
-        error,
-      };
-
-      lru.update(entry, failureResult);
-      return failureResult;
-    },
-  );
-
-  const pendingResult = {
-    status: RESOURCE_PENDING,
-    listen: cb => {
-      let isCanceled = false;
-
-      resultPromise.then(result => {
-        if (!isCanceled) {
-          cb(result);
-        }
+    if (result.status === RESOURCE_PENDING) {
+      return new Promise(resolve => {
+        result.listen(() => {
+          resolve();
+        });
       });
+    } else {
+      return result;
+    }
+  }
 
-      return () => {
-        isCanceled = true;
-      };
-    },
+  return {
+    [IS_RESOURCE_CACHE]: true,
+    [RESOURCE_CACHE_FETCH]: fetch,
+    preload,
   };
-
-  const entry = lru.add(pendingResult, () => resourceEntries.delete(hash));
-  resourceEntries.set(hash, entry);
-  return pendingResult;
-}
-
-function preloadEntry(entries, lru, resource, input) {
-  const result = fetchEntry(entries, lru, resource, input);
-
-  if (result.status === RESOURCE_PENDING) {
-    return new Promise(resolve => {
-      result.listen(() => {
-        resolve();
-      });
-    });
-  } else {
-    return result;
-  }
 }
