f1f950a19e904a2e1a9fe00f9c49759de1a852a2
Jérôme Guyon <jeromep.guyon@gmail.com>
2019-04-24 16:56:13 +0200

    Add a resource cache for use in tests

---
+++ src/resource-cache/create-test-resource-cache.js
@@ -0,0 +1,149 @@
+import invariant from "tiny-invariant";
+import {
+  IS_RESOURCE,
+  RESOURCE_HASH,
+  IS_RESOURCE_CACHE,
+  RESOURCE_CACHE_FETCH,
+  RESOURCE_PENDING,
+  RESOURCE_SUCCESS,
+  RESOURCE_FAILURE,
+} from "./constants";
+
+export default function createTestResourceCache() {
+  const entries = new Map();
+
+  function fetch(resource, input) {
+    invariant(
+      resource && typeof resource === "object" && resource[IS_RESOURCE],
+      "expected resource to be an object returned by `createResource`",
+    );
+
+    const resourceEntries = getResourceEntries(resource);
+    const hash = resource[RESOURCE_HASH](input);
+    const entry = resourceEntries.get(hash);
+
+    if (entry) {
+      return entry;
+    } else {
+      return createEntry(resourceEntries, hash);
+    }
+  }
+
+  function createEntry(resourceEntries, hash) {
+    let subscribers = [];
+
+    function listen(cb) {
+      if (subscribers) {
+        subscribers.push(cb);
+      }
+
+      return () => {
+        if (subscribers) {
+          const index = subscribers.indexOf(cb);
+          if (index > -1) {
+            subscribers.splice(index, 1);
+          }
+        }
+      };
+    }
+
+    function succeed(data) {
+      if (subscribers) {
+        const successEntry = {
+          status: RESOURCE_SUCCESS,
+          data,
+        };
+
+        resourceEntries.set(hash, successEntry);
+        for (const cb of subscribers) {
+          cb(successEntry);
+        }
+        subscribers = null;
+      }
+    }
+
+    function fail(error) {
+      if (subscribers) {
+        const failureEntry = {
+          status: RESOURCE_FAILURE,
+          error,
+        };
+
+        resourceEntries.set(hash, failureEntry);
+        for (const cb of subscribers) {
+          cb(failureEntry);
+        }
+        subscribers = null;
+      }
+    }
+
+    const pendingEntry = {
+      status: RESOURCE_PENDING,
+      listen,
+      succeed,
+      fail,
+    };
+
+    resourceEntries.set(hash, pendingEntry);
+    return pendingEntry;
+  }
+
+  function preload(resource, input) {
+    const entry = fetch(resource, input);
+
+    if (entry.status === RESOURCE_PENDING) {
+      return new Promise(resolve => {
+        entry.listen(() => {
+          resolve();
+        });
+      });
+    } else {
+      return Promise.resolve();
+    }
+  }
+
+  function succeed(resource, input, data) {
+    const entry = getPendingEntry(resource, input);
+    entry.succeed(data);
+  }
+
+  function fail(resource, input, error) {
+    const entry = getPendingEntry(resource, input);
+    entry.fail(error);
+  }
+
+  function getPendingEntry(resource, input) {
+    const resourceEntries = getResourceEntries(resource);
+    const hash = resource[RESOURCE_HASH](input);
+    const entry = resourceEntries.get(hash);
+
+    invariant(entry, "expected resource to have been fetched with given input");
+    invariant(
+      entry.status === RESOURCE_PENDING,
+      "expected resource to not have been succeeded or failed " +
+        "with given input before",
+    );
+
+    return entry;
+  }
+
+  function getResourceEntries(resource) {
+    const resourceEntries = entries.get(resource);
+
+    if (resourceEntries) {
+      return resourceEntries;
+    } else {
+      const newResourceEntries = new Map();
+      entries.set(resource, newResourceEntries);
+      return newResourceEntries;
+    }
+  }
+
+  return {
+    [IS_RESOURCE_CACHE]: true,
+    [RESOURCE_CACHE_FETCH]: fetch,
+    preload,
+    succeed,
+    fail,
+  };
+}
---
+++ src/resource-cache/create-test-resource-cache.test.js
@@ -0,0 +1,337 @@
+import React from "react";
+import invariant from "tiny-invariant";
+import { render, act } from "../test/react";
+import {
+  RESOURCE_PENDING,
+  RESOURCE_SUCCESS,
+  RESOURCE_FAILURE,
+} from "./constants";
+import createResource from "./create-resource";
+import useResource, { ResourceCacheProvider } from "./use-resource";
+import createTestResourceCache from "./create-test-resource-cache";
+
+function fetch() {
+  invariant(false, "resource fetch function should not be called");
+}
+
+function AsyncResource({ resource, input }) {
+  const result = useResource(resource, input);
+
+  switch (result.status) {
+    case RESOURCE_PENDING:
+      return <>pending</>;
+    case RESOURCE_SUCCESS:
+      return <>success: {result.data}</>;
+    case RESOURCE_FAILURE:
+      return <>failure: {result.error}</>;
+    default:
+      invariant(false, `invalid status ${result.status}`);
+  }
+}
+
+test("pending result is rendered when requested resource is not in cache", () => {
+  const resource = createResource(fetch);
+  const cache = createTestResourceCache();
+
+  const { container } = render(
+    <ResourceCacheProvider cache={cache}>
+      <AsyncResource resource={resource} input="input" />
+    </ResourceCacheProvider>,
+  );
+
+  expect(container).toHaveTextContent("pending");
+});
+
+test("pending result is rendered when requested resource is currently pending", () => {
+  const resource = createResource(fetch);
+  const cache = createTestResourceCache();
+
+  const { container, rerender } = render(
+    <ResourceCacheProvider cache={cache}>
+      <AsyncResource key="one" resource={resource} input="input" />
+    </ResourceCacheProvider>,
+  );
+
+  expect(container).toHaveTextContent("pending");
+
+  rerender(
+    <ResourceCacheProvider cache={cache}>
+      <AsyncResource key="two" resource={resource} input="input" />
+    </ResourceCacheProvider>,
+  );
+
+  expect(container).toHaveTextContent("pending");
+});
+
+test("successful result is rendered when requested resource is succeeded", () => {
+  const resource = createResource(fetch);
+  const cache = createTestResourceCache();
+
+  const { container } = render(
+    <ResourceCacheProvider cache={cache}>
+      <AsyncResource resource={resource} input="input" />
+    </ResourceCacheProvider>,
+  );
+
+  act(() => {
+    cache.succeed(resource, "input", "resource data");
+  });
+
+  expect(container).toHaveTextContent("success: resource data");
+});
+
+test("successful result is rendered when requested resource has previously been succeeded", () => {
+  const resource = createResource(fetch);
+  const cache = createTestResourceCache();
+
+  const { container, rerender } = render(
+    <ResourceCacheProvider cache={cache}>
+      <AsyncResource key="one" resource={resource} input="input" />
+    </ResourceCacheProvider>,
+  );
+
+  act(() => {
+    cache.succeed(resource, "input", "resource data");
+  });
+
+  rerender(
+    <ResourceCacheProvider cache={cache}>
+      <AsyncResource key="two" resource={resource} input="input" />
+    </ResourceCacheProvider>,
+  );
+
+  expect(container).toHaveTextContent("success: resource data");
+});
+
+test("failed result is rendered when requested resource is failed", () => {
+  const resource = createResource(fetch);
+  const cache = createTestResourceCache();
+
+  const { container } = render(
+    <ResourceCacheProvider cache={cache}>
+      <AsyncResource resource={resource} input="input" />
+    </ResourceCacheProvider>,
+  );
+
+  act(() => {
+    cache.fail(resource, "input", "resource error");
+  });
+
+  expect(container).toHaveTextContent("failure: resource error");
+});
+
+test("failed result is rendered when requested resource has previously been failed", () => {
+  const resource = createResource(fetch);
+  const cache = createTestResourceCache();
+
+  const { container, rerender } = render(
+    <ResourceCacheProvider cache={cache}>
+      <AsyncResource key="one" resource={resource} input="input" />
+    </ResourceCacheProvider>,
+  );
+
+  act(() => {
+    cache.fail(resource, "input", "resource error");
+  });
+
+  rerender(
+    <ResourceCacheProvider cache={cache}>
+      <AsyncResource key="two" resource={resource} input="input" />
+    </ResourceCacheProvider>,
+  );
+
+  expect(container).toHaveTextContent("failure: resource error");
+});
+
+test("result is rerendered when new input is requested", () => {
+  const resource = createResource(fetch);
+  const cache = createTestResourceCache();
+
+  const { container, rerender } = render(
+    <ResourceCacheProvider cache={cache}>
+      <AsyncResource resource={resource} input="one" />
+    </ResourceCacheProvider>,
+  );
+
+  act(() => {
+    cache.succeed(resource, "one", "resource data one");
+  });
+
+  expect(container).toHaveTextContent("success: resource data one");
+
+  rerender(
+    <ResourceCacheProvider cache={cache}>
+      <AsyncResource resource={resource} input="two" />
+    </ResourceCacheProvider>,
+  );
+
+  expect(container).toHaveTextContent("pending");
+
+  act(() => {
+    cache.succeed(resource, "two", "resource data two");
+  });
+
+  expect(container).toHaveTextContent("success: resource data two");
+});
+
+test("result is rerendered when new resource is requested", () => {
+  const resourceOne = createResource(fetch);
+  const resourceTwo = createResource(fetch);
+  const cache = createTestResourceCache();
+
+  const { container, rerender } = render(
+    <ResourceCacheProvider cache={cache}>
+      <AsyncResource resource={resourceOne} input="input" />
+    </ResourceCacheProvider>,
+  );
+
+  act(() => {
+    cache.succeed(resourceOne, "input", "resource data one");
+  });
+
+  expect(container).toHaveTextContent("success: resource data one");
+
+  rerender(
+    <ResourceCacheProvider cache={cache}>
+      <AsyncResource resource={resourceTwo} input="input" />
+    </ResourceCacheProvider>,
+  );
+
+  expect(container).toHaveTextContent("pending");
+
+  act(() => {
+    cache.succeed(resourceTwo, "input", "resource data two");
+  });
+
+  expect(container).toHaveTextContent("success: resource data two");
+});
+
+test("result is rerendered when new cache is provided", () => {
+  const resource = createResource(fetch);
+  const cacheOne = createTestResourceCache();
+  const cacheTwo = createTestResourceCache();
+
+  const { container, rerender } = render(
+    <ResourceCacheProvider cache={cacheOne}>
+      <AsyncResource resource={resource} input="input" />
+    </ResourceCacheProvider>,
+  );
+
+  act(() => {
+    cacheOne.succeed(resource, "input", "resource data one");
+  });
+
+  expect(container).toHaveTextContent("success: resource data one");
+
+  rerender(
+    <ResourceCacheProvider cache={cacheTwo}>
+      <AsyncResource resource={resource} input="input" />
+    </ResourceCacheProvider>,
+  );
+
+  expect(container).toHaveTextContent("pending");
+
+  act(() => {
+    cacheTwo.succeed(resource, "input", "resource data two");
+  });
+
+  expect(container).toHaveTextContent("success: resource data two");
+});
+
+test("previous fetching is canceled when refetching", () => {
+  const resource = createResource(fetch);
+  const cache = createTestResourceCache();
+
+  const { container, rerender } = render(
+    <ResourceCacheProvider cache={cache}>
+      <AsyncResource resource={resource} input="one" />
+    </ResourceCacheProvider>,
+  );
+
+  expect(container).toHaveTextContent("pending");
+
+  rerender(
+    <ResourceCacheProvider cache={cache}>
+      <AsyncResource resource={resource} input="two" />
+    </ResourceCacheProvider>,
+  );
+
+  expect(container).toHaveTextContent("pending");
+
+  act(() => {
+    cache.succeed(resource, "one", "resource data one");
+  });
+
+  expect(container).toHaveTextContent("pending");
+
+  act(() => {
+    cache.succeed(resource, "two", "resource data two");
+  });
+
+  expect(container).toHaveTextContent("success: resource data two");
+});
+
+test("input is hashed with provided hash function", () => {
+  const resource = createResource(fetch, ({ text }) => text);
+  const cache = createTestResourceCache();
+
+  const { container, rerender } = render(
+    <ResourceCacheProvider cache={cache}>
+      <AsyncResource
+        resource={resource}
+        input={{ text: "text", other: "one" }}
+      />
+    </ResourceCacheProvider>,
+  );
+
+  act(() => {
+    cache.succeed(resource, { text: "text" }, "resource data text");
+  });
+
+  expect(container).toHaveTextContent("success: resource data text");
+
+  rerender(
+    <ResourceCacheProvider cache={cache}>
+      <AsyncResource
+        resource={resource}
+        input={{ text: "text", other: "two" }}
+      />
+    </ResourceCacheProvider>,
+  );
+
+  expect(container).toHaveTextContent("success: resource data text");
+});
+
+test("successful result is rendered when requested resource has been succeeded during preloading", async () => {
+  const resource = createResource(fetch);
+  const cache = createTestResourceCache();
+
+  const promise = cache.preload(resource, "input");
+  cache.succeed(resource, "input", "resource data");
+  await promise;
+
+  const { container } = render(
+    <ResourceCacheProvider cache={cache}>
+      <AsyncResource resource={resource} input="input" />
+    </ResourceCacheProvider>,
+  );
+
+  expect(container).toHaveTextContent("success: resource data");
+});
+
+test("failed result is rendered when requested resource has been failed during preloading", async () => {
+  const resource = createResource(fetch);
+  const cache = createTestResourceCache();
+
+  const promise = cache.preload(resource, "input");
+  cache.fail(resource, "input", "resource error");
+  await promise;
+
+  const { container } = render(
+    <ResourceCacheProvider cache={cache}>
+      <AsyncResource resource={resource} input="input" />
+    </ResourceCacheProvider>,
+  );
+
+  expect(container).toHaveTextContent("failure: resource error");
+});
