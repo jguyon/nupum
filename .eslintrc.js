76e82617b8bdd4e4e21e054f536384f92f22a46a
Jérôme Guyon <jeromep.guyon@gmail.com>
2019-05-01 20:45:50 +0200

    Upgrade eslint-config-react-app

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,6 @@
 module.exports = {
   extends: ["react-app"],
-  plugins: ["react-hooks"],
   rules: {
-    "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
   },
   globals: {
