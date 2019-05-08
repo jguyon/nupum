7cb114eaa9aea00223f9e9b8b69635723563cf0a
Jérôme Guyon <jeromep.guyon@gmail.com>
2019-05-08 14:36:09 +0200

    Fix generation of file hashes for production
    Building for production was producing files with different hashes
    depending on the absolute path of the project. This was breaking
    deployment because the static files are built once for the static
    builder to serve them, and once for the node builder to retrieve the url
    of the files to serve, but both builds were generating different file
    names.

--- .babelrc.js
+++ .babelrc.js
@@ -1,4 +1,14 @@
 module.exports = {
-  presets: ["react-app", "@emotion/babel-preset-css-prop"],
+  presets: [
+    // Activating absoluteRuntime creates a variable which contains an absolute
+    // path, which means the contents of files are different on different
+    // machines and therefore create different file hashes.
+    ["react-app", { absoluteRuntime: false }],
+    // Similarly, activating sourceMap also produces different files on
+    // different machines which leads to the same problem.
+    // Additionally, they produce dom mismatches because the generated classes
+    // are not the same in the client build and the server build.
+    ["@emotion/babel-preset-css-prop", { sourceMap: false }],
+  ],
   plugins: ["babel-plugin-dev-expression"],
 };
