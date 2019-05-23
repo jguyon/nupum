16916a8b830ec15281fd4b6ab3de66adeb9796ba
Jérôme Guyon <jeromep.guyon@gmail.com>
2019-05-23 18:42:25 +0200

    Simplify syntax highlighting
    There is no need to manage dependencies of languages, refractor already
    handles that.

--- src/app/package-page/package-page-readme-language-modules.js
+++ src/app/package-page/package-page-readme-language-modules.js
@@ -2,42 +2,35 @@ import React from "react";
 import { createModule } from "../../module-cache";
 
 const SUPPORTED_LANGUAGES = [
-  { lang: "coffeescript", deps: ["javascript"], aliases: ["coffee"] },
-  { lang: "css", deps: ["markup"], aliases: [] },
-  { lang: "elm", deps: [], aliases: [] },
-  { lang: "flow", deps: ["clike", "markup", "javascript"], aliases: [] },
-  { lang: "graphql", deps: [], aliases: ["gql"] },
-  { lang: "javascript", deps: ["clike", "markup"], aliases: ["js"] },
-  { lang: "jsx", deps: ["clike", "markup", "javascript"], aliases: [] },
-  { lang: "markdown", deps: ["markup"], aliases: ["md"] },
-  { lang: "markup", deps: [], aliases: ["html", "xml", "svg"] },
-  { lang: "ocaml", deps: [], aliases: [] },
-  { lang: "reason", deps: ["clike"], aliases: [] },
-  { lang: "sass", deps: ["markup", "css"], aliases: [] },
-  { lang: "scss", deps: ["markup", "css"], aliases: [] },
-  { lang: "sql", deps: [], aliases: [] },
-  { lang: "tsx", deps: ["clike", "markup", "javascript"], aliases: [] },
-  {
-    lang: "typescript",
-    deps: ["clike", "markup", "javascript"],
-    aliases: ["ts"],
-  },
-  { lang: "yaml", deps: [], aliases: ["yml"] },
+  { lang: "coffeescript", aliases: ["coffee"] },
+  { lang: "css", aliases: [] },
+  { lang: "elm", aliases: [] },
+  { lang: "flow", aliases: [] },
+  { lang: "graphql", aliases: ["gql"] },
+  { lang: "javascript", aliases: ["js"] },
+  { lang: "jsx", aliases: [] },
+  { lang: "markdown", aliases: ["md"] },
+  { lang: "markup", aliases: ["html", "xml", "svg"] },
+  { lang: "ocaml", aliases: [] },
+  { lang: "reason", aliases: [] },
+  { lang: "sass", aliases: [] },
+  { lang: "scss", aliases: [] },
+  { lang: "sql", aliases: [] },
+  { lang: "tsx", aliases: [] },
+  { lang: "typescript", aliases: ["ts"] },
+  { lang: "yaml", aliases: ["yml"] },
 ];
 
 const languageModules = new Map();
 
-for (const { lang, deps, aliases } of SUPPORTED_LANGUAGES) {
+for (const { lang, aliases } of SUPPORTED_LANGUAGES) {
   const module = createModule(async () => {
-    const [Refractor, ...grammars] = await Promise.all([
+    const [Refractor, grammar] = await Promise.all([
       importRefractor(),
-      ...deps.map(dep => importLanguage(dep)),
       importLanguage(lang),
     ]);
 
-    for (const grammar of grammars) {
-      Refractor.registerLanguage(grammar);
-    }
+    Refractor.registerLanguage(grammar);
 
     return function HighlightLanguage(props) {
       return <Refractor {...props} language={lang} />;
